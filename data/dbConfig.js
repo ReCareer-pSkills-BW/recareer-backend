852a5775c901e23e5ba7c439e5dbcf3dc9de94cb
melissa-murphy <melissamurphy.ca@gmail.com>
2019-10-24 22:04:17 -0700

    env production

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,6 +1,6 @@
 // DB Config
 const knex = require('knex');
 const config = require('../knexfile.js');
-const env = process.env.DB_ENV || 'development';
+const env = process.env.DB_ENV || 'production';
 
 module.exports = knex(config[env]);
